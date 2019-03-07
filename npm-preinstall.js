bf89dbb371a5e97881868c5dba152562e0070cd3
Víctor Navarro <vicnala@gmail.com>
2019-03-07 16:21:20 +0100

    Info the user about installing libsystemd-dev

--- npm-preinstall.js
+++ npm-preinstall.js
@@ -2,6 +2,8 @@
 
 const cp = require('child_process');
 
+console.info("Installing libsystemd-dev ...");
+
 const installed = cp.spawnSync('sudo', ['apt-get', 'install', '-qq', '--yes', 'libsystemd-dev']);
 
 if (installed.status !== 0) {
